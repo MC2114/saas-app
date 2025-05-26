18f281403ce1ac3c0a461f02c6a1d142684a9868
Chau Hoang Minh Tran <chtran@umass.edu>
2025-05-25 20:41:18 -0400

    Implement NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import {Button} from "@/components/ui/button";
 
 const Page = () => {
   return (
       <div>
-        <h1 className='text-2xl underline'>Hello</h1>
-        <Button>
-            Yipee Kai Yay such Shadcn
-        </Button>
+
       </div>
   )
 }
